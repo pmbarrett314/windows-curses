a40312f1f51334dab9976a4d1a5264e68fcaf29c
Paul Barrett <pmbarrett314@gmail.com>
2024-10-14 21:12:08 -0500

    change define

--- py313/_cursesmodule.c
+++ py313/_cursesmodule.c
@@ -100,9 +100,7 @@ static const char PyCursesVersion[] = "2.2";
 
 /* Includes */
 
-#ifndef Py_BUILD_CORE_BUILTIN
-#  define Py_BUILD_CORE_MODULE 1
-#endif
+#define PY_SSIZE_T_CLEAN
 
 #include "Python.h"
 
